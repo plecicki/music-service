21098e7a623e9906e39a40ded5cfd3d1d875c695
Piotr Lecicki <p.lecicki@gmail.com>
2025-02-24 22:41:07 +0100

    Add pages

---
+++ src/js/app.js
@@ -0,0 +1,48 @@
+import { select, classNames } from './settings.js';
+import { appAdvice } from './appAdvice.js';
+
+const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if(page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      appAdvice.configureNavLink(thisApp, link);
+    }
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+  init: function () {
+    const thisApp = this;
+    console.log('*** App starting ***');
+    thisApp.initPages();
+  },
+};
+
+app.init();
---
+++ src/js/settings.js
@@ -0,0 +1,17 @@
+export const select = {
+  containerOf: {
+    pages: '#pages',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
+};
+
+export const classNames = {
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  }
+};
